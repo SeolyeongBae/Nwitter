edfdd59c80b6740510ddcaf7352df7ddcf33c753
pell13 <peixueying@gmail.com>
2021-10-13 18:59:05 +0900

    Add .env to hide key

--- nwitter/src/firebase.js
+++ nwitter/src/firebase.js
@@ -1,12 +1,12 @@
-import firebase from "firebase/app";
+import firebase from "firebase/compat/app";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCCtIkoUa5ERZv2L0EewHpohyh_qc4Ijiw",
-    authDomain: "nwitter-df7ea.firebaseapp.com",
-    projectId: "nwitter-df7ea",
-    storageBucket: "nwitter-df7ea.appspot.com",
-    messagingSenderId: "281230443298",
-    appId: "1:281230443298:web:7521c9a6ee34fa44b2db7b"
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGIN_ID,
+    appId: process.env.REACT_APP_APP_ID 
   };
 
   export default firebase.initializeApp(firebaseConfig);
