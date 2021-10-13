0224eb6decd460f297eea161583840484ed3752b
pell13 <peixueying@gmail.com>
2021-10-13 17:12:27 +0900

    Setup Firebase

---
+++ nwitter/src/firebase.js
@@ -0,0 +1,12 @@
+import firebase from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCCtIkoUa5ERZv2L0EewHpohyh_qc4Ijiw",
+    authDomain: "nwitter-df7ea.firebaseapp.com",
+    projectId: "nwitter-df7ea",
+    storageBucket: "nwitter-df7ea.appspot.com",
+    messagingSenderId: "281230443298",
+    appId: "1:281230443298:web:7521c9a6ee34fa44b2db7b"
+  };
+
+  export default firebase.initializeApp(firebaseConfig);
