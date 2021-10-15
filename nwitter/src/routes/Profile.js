0e06bc9bb2a5faa2d3495bbd40c9ade94d266036
pell13 <peixueying@gmail.com>
2021-10-15 17:36:07 +0900

    Add Log Out

--- nwitter/src/routes/Profile.js
+++ nwitter/src/routes/Profile.js
@@ -1,3 +1,16 @@
+import { authService } from "fBase";
 import React from "react";
+import { useHistory } from "react-router";
 
-export default () => <span>Profile</span>;
+export default () => {
+  const history = useHistory();
+  const onLogOutClick = () => {
+    history.push("/");
+    authService.signOut();
+  };
+  return (
+    <>
+      <button onClick={onLogOutClick}>Log out</button>
+    </>
+  );
+};
